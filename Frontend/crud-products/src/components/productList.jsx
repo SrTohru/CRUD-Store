618aa9ac534b4be7e1200b3a9a1a42f2fc6bc29b
Jose Hinojosa <74486457+SrTohru@users.noreply.github.com>
2025-11-04 16:58:07 -0700

    Se inicio con el diseño frontend y css con tailwind

--- Frontend/crud-products/src/components/productList.jsx
+++ Frontend/crud-products/src/components/productList.jsx
@@ -1,18 +1,35 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { getProducts } from "../api/products";
 
 export default function ProductList() {
 
+    const [products, setProducts] = useState([]);
+
     const loadedProducts = async () => {
         const response = await getProducts();
-        console.log(response);
+        setProducts(response);
     }
 
-    loadedProducts();
+    useEffect(() => {
+        loadedProducts();
+    }, []);
+
   return (
-    <div>
+    <div className="mt-10">
         
-      <h2>Product List</h2>
+      <h1 className="text-3x1 font-bold text-sky-900">Productos en disponibilidad</h1>
     
+      <div className="grid grid-cols-1 md:grid-cols-3 mt-3 gap-4"> 
+        { products.map( product => (
+          <div key={product.id} className="bg-amber-200 p-4 rounded-lg shadow">
+            <p>{product.nombre}</p>
+            <p>{product.descripcion}</p>
+            <p>{product.precio}</p>
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 }
